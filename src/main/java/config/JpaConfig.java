1690697654b339863b9455acb61fff10e6ffec3e
Ben Miller <miller.bennett@gmail.com>
2016-04-02 20:45:34 -0400

    Improving jpa configuration, no longer instantiating entity manager. Configuration to show sql in log.

--- src/main/java/config/JpaConfig.java
+++ src/main/java/config/JpaConfig.java
@@ -1,6 +1,6 @@
 package config;
 
-import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 import org.postgresql.Driver;
@@ -8,9 +8,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
-import org.springframework.orm.jpa.EntityManagerFactoryInfo;
 import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -27,25 +28,27 @@ public class JpaConfig {
 	}
 	
 	@Bean
-	public EntityManager entityManager() {
-		return (EntityManager)entityManagerFactory()
-				.getNativeEntityManagerFactory().createEntityManager();
-	}
-	
-	@Bean
     public JpaTransactionManager transactionManager() {
         JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(entityManagerFactory().getNativeEntityManagerFactory());
+        transactionManager.setEntityManagerFactory(entityManagerFactory());
         return transactionManager;
     }
 
 	@Bean
-	public EntityManagerFactoryInfo entityManagerFactory() {
-		LocalContainerEntityManagerFactoryBean bean = 
+	public EntityManagerFactory entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean factory = 
 				new LocalContainerEntityManagerFactoryBean();
-		bean.setDataSource(dataSource());
-		bean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-		bean.setPackagesToScan("modules.entities");
-		return bean;
+		factory.setDataSource(dataSource());
+		factory.setJpaVendorAdapter(adapter());
+		factory.setPackagesToScan("modules.entities");
+		factory.afterPropertiesSet();
+		return factory.getObject();
+	}
+	
+	private JpaVendorAdapter adapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.POSTGRESQL);
+		adapter.setShowSql(true);
+		return adapter;
 	}
 }
